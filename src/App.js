c08f88dcc2529e783a693fb750c2feebb284f866
PoststresSQL <Adamwage@hotmail.com>
2020-01-18 16:56:00 -0600

    Tiles click and update state.

--- src/App.js
+++ src/App.js
@@ -19,3 +19,48 @@ function App() {
 }
 
 export default App;
+
+/*
+SINGLE_ARRAY
+
+
+is this an end of row?
+  idx%5 === 0
+is the beginning of row?
+  (idx-1)%5 === 0
+
+cols = 5
+rows = 5
+
+is in first row?
+  idx <= rows
+is in last row?
+  idx <= (cols * rows) && idx > ((cols * rows) - rows)
+
+1 2 3 4 5
+x x x x x
+6 7 8 9 0
+x x x x x
+1 2 3 4 5
+x x x x x
+6 7 8 9 0
+x x x x x
+1 2 3 4 5
+x x x x x
+
+*/
+
+/*
+
+MATRIX
+
+[
+  [0w, 1w, 2w, 3w, 4w], //0
+  [0v, 1v, 2v, 3v, 4v], //1
+  [0x, 1x, 2x, 3x, 4x], //2
+  [0y, 1y, 2y, 3y, 4y], //3
+  [0z, 1z, 2z, 3z, 4z], //4
+]
+
+
+*/
--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './tile.css';
 
-const Tile = ({ id }) => (
-  <div id={id} className={'tile'}></div>
-);
+const Tile = ({ id }) => {
+  const [litEh, toggleLit] = useState(false);
+  return (
+    <div
+      id={id}
+      className={litEh ? 'tile lit' : 'tile'}
+      onClick={() => toggleLit(!litEh)}
+    >
+    </div>
+  )
+};
 
 export default Tile;
