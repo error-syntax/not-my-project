4bb7ff141f7c9f3db70e2d52d434f6f583afde00
Buford Eeds <bufordeeds8@gmail.com>
2020-01-20 12:34:19 -0600

    formatting

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
-import Button from './components/Button';
-import Modal from './containers/Modal';
-import TilesContainer from './containers/Tiles';
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Modal from "./containers/Modal";
+import TilesContainer from "./containers/Tiles/TileContainer";
 
 function App() {
   const [rows, setRows] = useState(5);
   const [cols, setCols] = useState(5);
 
   const changeGrid = () => {
-    const newGridSize = +document.querySelector('#gridSize__input').value;
-    
+    const newGridSize = +document.querySelector("#gridSize__input").value;
+
     setRows(newGridSize);
     setCols(newGridSize);
-    document.querySelector('#game-modal__container').classList.remove('show');
+    document.querySelector("#game-modal__container").classList.remove("show");
   };
 
   return (
     <div className="App">
-      <Modal id={'game-modal__container'} className='modal__container'>
+      <Modal id={"game-modal__container"} className="modal__container ">
         <h2>Enter the number of ROWSxCOLS you'd like your puzzle.</h2>
         <span>
-          <input id={'gridSize__input'} type={'text'} placeholder={'Grid Size?'} />
-          <Button className='btn--light' handleClick={changeGrid}>
+          <input
+            id={"gridSize__input"}
+            type={"text"}
+            placeholder={"Grid Size?"}
+          />
+          <Button className="btn--light" handleClick={changeGrid}>
             Submit
           </Button>
         </span>
       </Modal>
-      <TilesContainer
-        cols={cols}
-        rows={rows}
-      />
+      <TilesContainer cols={cols} rows={rows} />
     </div>
-  )
+  );
 }
 
 export default App;
