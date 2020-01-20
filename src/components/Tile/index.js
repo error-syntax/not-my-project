b3c8dfbf1369461af9c5ba959324db0ccaeb5093
PoststresSQL <adamwage@hotmail.com>
2020-01-19 23:59:14 -0600

    Co-authored-by: Buford Eeds <bufordeeds8@gmail.com> TilesContainer handles State for each tile instead of tile keeping track.

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './tile.css';
 
-const Tile = ({ id }) => {
-  const [litEh, toggleLit] = useState(false);
-  return (
-    <div
-      id={id}
-      className={litEh ? 'tile lit' : 'tile'}
-      onClick={() => toggleLit(!litEh)}
-    >
-    </div>
-  )
+const Tile = ({id, litEh, toggleLit}) => {
+    return (
+      <div
+        id={id}
+        className={litEh === 1 ? 'tile lit' : 'tile'}
+        onClick={() => {
+          toggleLit(id)
+        }}
+      >
+      </div>
+    )
 };
 
 export default Tile;
+
+/*
+
+MATRIX
+
+[
+  [0w, 1w, 2w, 3w, 4w], //0
+  [0v, 1v, 2v, 3v, 4v], //1
+  [0x, 1x, 2x, 3x, 4x], //2
+  [0y, 1y, 2y, 3y, 4y], //3
+  [0z, 1z, 2z, 3z, 4z], //4
+]
+
+
+*/
--- src/containers/Tiles/index.js
+++ src/containers/Tiles/index.js
@@ -1,21 +1,102 @@
 import React from 'react';
+import Tile from '../../components/Tile';
 import './tiles.css'
 
-const TilesContainer = ({ children, cols, rows }) => { 
-  const styles = {
-    gridTemplateColumns: `repeat(${cols}, 3.6rem)`,
-    gridTemplateRows: `repeat(${rows}, 3.6rem)`
-  };
+class TilesContainer extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      cols: 0,
+      litEhStateArr: [],
+      rows: 0,
+    }
+  }
 
-  return (
-    <div 
-      className={'grid'}
-      id={'tiles__container'}
-      style={styles}
-    >
-      {children}
-    </div>
-  )
+  buildStateArr = (cols, rows) => {
+    let builderStateArr = []
+
+    for (let i = 0; i < rows; i++) {
+      builderStateArr.push([]);
+      for (let j = 0; j < cols; j++) {
+        builderStateArr[i].push(0)
+      }
+    }
+
+    this.setState({
+      cols: cols,
+      rows: rows,
+      litEhStateArr: builderStateArr
+    })
+  }
+ 
+  toggleLit = (tileId) => {
+    const rowNum = tileId.split("")[0]
+    const colNum = tileId.split("")[1]
+
+    const newStateOfTile = this.state.litEhStateArr
+    newStateOfTile[rowNum][colNum] = 1
+
+    this.setState({
+      ...this.state,
+      litEhStateArr: newStateOfTile
+    })
+  }
+
+  componentDidMount() {
+    const {cols, rows} = this.props
+    this.buildStateArr(cols, rows);
+  }
+
+  render() {
+    const styles = {
+      gridTemplateColumns: `repeat(${this.props.cols}, 3.6rem)`,
+      gridTemplateRows: `repeat(${this.props.rows}, 3.6rem)`
+    };    
+
+    const {
+      litEhStateArr
+    } = this.state
+
+    return (
+      <div
+        className={'grid'}
+        id={'tiles__container'}
+        style={styles}
+      >
+        {
+          Array.from(Array(this.props.rows)).map((_tile, rowNum) => {
+            return Array.from(Array(this.props.cols)).map((_tile, colNum) => {
+              return (
+                <Tile
+                  key={`${rowNum}${colNum}`}
+                  id={`${rowNum}${colNum}`}
+                  litEh={litEhStateArr[rowNum] && litEhStateArr[rowNum][colNum]}
+                  toggleLit={this.toggleLit}
+                />
+              );
+            });
+          })
+        }
+      </div>
+    )
+  }
 };
 
 export default TilesContainer;
+
+
+
+/*
+
+LitEh MATRIX
+
+[  0  1  2  3  4
+  [0, 0, 0, 0, 0], //0
+  [0, 0, 0, 0, 0], //1
+  [0, 0, 0, 0, 0], //2
+  [0, 0, 0, 0, 0], //3
+  [0, 0, 0, 0, 0], //4
+]
+
+
+*/
