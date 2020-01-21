c495de01238bdaa9da78fcfbcb012e1e6b1f4616
Buford Eeds <bufordeeds8@gmail.com>
2020-01-21 00:35:47 -0600

    stated building homepage

---
+++ src/views/Homepage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './homepage.css'
+
+const Hompage = () => {
+  return (
+    <div>
+      <h1>
+        Home Route
+      </h1>
+    </div>
+  );
+};
+
+export default Hompage;
