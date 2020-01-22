3ba725427bb0099f2899dba00784b8b1750c65eb
Buford Eeds <bufordeeds8@gmail.com>
2020-01-22 14:32:52 -0600

    added boiler error text

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
 
-export const SignUpForm = ({errors, handleErrors, handleSubmit }) => (
+export const SignUpForm = ({ errors, handleErrors, handleSubmit }) => (
   <div className="form-container sign-up-container">
     <form id={"sign-up__form"}>
       <h1>Create Account</h1>
@@ -11,14 +10,18 @@ export const SignUpForm = ({errors, handleErrors, handleSubmit }) => (
         type="text"
         placeholder="Name"
       />
-      <span className={"error__container"}>{}</span>
+      <span className={"error__container"}>
+        {errors + " message goes here"}
+      </span>
       <input
         className="input"
         data-colname={"password"}
         type="password"
         placeholder="Password"
       />
-      <span className={"error__container"}>{}</span>
+      <span className={"error__container"}>
+        {errors + " message goes here"}
+      </span>
       <button
         onClick={e => {
           e.preventDefault();
@@ -27,37 +30,34 @@ export const SignUpForm = ({errors, handleErrors, handleSubmit }) => (
       >
         Sign Up
       </button>
-      <Link to="/">
-        <button>Go Back</button>
-      </Link>
     </form>
   </div>
 );
 
 export const SignInForm = ({ errors, handleErrors, handleSubmit }) => (
-         <div className="form-container sign-in-container">
-           <form id={"sign-in__form"}>
-             <h1>Sign in</h1>
-             <input
-               className="input"
-               data-colname={"username"}
-               type="name"
-               placeholder="Name"
-             />
-             <input
-               className="input"
-               data-colname={"password"}
-               type="password"
-               placeholder="Password"
-             />
-             <button
-               onClick={e => {
-                 e.preventDefault();
-                 handleSubmit("sign-in__form");
-               }}
-             >
-               Sign In
-             </button>
-           </form>
-         </div>
-       );
+  <div className="form-container sign-in-container">
+    <form id={"sign-in__form"}>
+      <h1>Sign in</h1>
+      <input
+        className="input"
+        data-colname={"username"}
+        type="name"
+        placeholder="Name"
+      />
+      <input
+        className="input"
+        data-colname={"password"}
+        type="password"
+        placeholder="Password"
+      />
+      <button
+        onClick={e => {
+          e.preventDefault();
+          handleSubmit("sign-in__form");
+        }}
+      >
+        Sign In
+      </button>
+    </form>
+  </div>
+);
