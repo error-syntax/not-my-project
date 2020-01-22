feb54b1f0ea2517874c53b909ec058bf51299014
Buford Eeds <bufordeeds8@gmail.com>
2020-01-22 14:35:37 -0600

    moved contents of index to file named after folder for better readability

--- src/containers/Modal/index.js
+++ src/containers/Modal/index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import './modal.css';
-
-const Modal = ({ children, className, id }) => (
-  <div id={id} className={className}>
-    {children}
-  </div>
-);
-
-export default Modal;
+export * from './Modal.js';
