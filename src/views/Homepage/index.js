a1f20f26b42f2d34a57ecc9aca76b7d6520907b6
Buford Eeds <bufordeeds8@gmail.com>
2020-01-22 14:36:28 -0600

    moved contents of index to file named after folder for better readability

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,14 +1,1 @@
-import React from "react";
-import './homepage.css'
-
-const Hompage = () => {
-  return (
-    <div>
-      <h1>
-        Home Route
-      </h1>
-    </div>
-  );
-};
-
-export default Hompage;
+export * from './Homepage.js';
