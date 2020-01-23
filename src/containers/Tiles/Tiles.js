88ee965463c1597941e38ddc04e97876ba4fe731
Buford Eeds <bufordeeds8@gmail.com>
2020-01-23 12:12:58 -0600

    added score state and game complete function

--- src/containers/Tiles/Tiles.js
+++ src/containers/Tiles/Tiles.js
@@ -1,108 +1,139 @@
-import React from 'react';
-import { Tile } from '../../components/Tile';
-import './tiles.css';
+import React from "react";
+import { Tile } from "../../components/Tile";
+import "./tiles.css";
 
 export class TilesContainer extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			cols: 0,
-			rows: 0,
-			litEhStateArr: [],
-			clicks: 0,
-			stopTimer: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      cols: 0,
+      rows: 0,
+      litEhStateArr: [],
+      clicks: 0,
+      stopTimer: false,
+      score: 0
+    };
+  }
 
-	buildStateArr = (cols, rows) => {
-		let builderStateArr = [];
+  buildStateArr = (cols, rows) => {
+    let builderStateArr = [];
 
-		for (let i = 0; i < rows; i++) {
-			builderStateArr.push([]);
-			for (let j = 0; j < cols; j++) {
-				builderStateArr[i].push(0);
-			}
-		}
+    for (let i = 0; i < rows; i++) {
+      builderStateArr.push([]);
+      for (let j = 0; j < cols; j++) {
+        builderStateArr[i].push(0);
+      }
+    }
 
-		this.setState({
-			cols: cols,
-			rows: rows,
-			litEhStateArr: builderStateArr
-		});
-	};
+    this.setState({
+      cols: cols,
+      rows: rows,
+      litEhStateArr: builderStateArr
+    });
+  };
 
-	toggleLit = (tileId) => {
-		const rowNum = +tileId.split('')[0];
-		const colNum = +tileId.split('')[1];
+  gameComplete = () => {
 
-		const newStateOfTiles = this.state.litEhStateArr;
-		newStateOfTiles[rowNum][colNum] = !newStateOfTiles[rowNum][colNum];
-		newStateOfTiles[rowNum + 1] &&
-			(newStateOfTiles[rowNum + 1][colNum] = !newStateOfTiles[rowNum + 1][
-				colNum
-			]);
-		newStateOfTiles[rowNum - 1] &&
-			(newStateOfTiles[rowNum - 1][colNum] = !newStateOfTiles[rowNum - 1][
-				colNum
-			]);
-		newStateOfTiles[colNum + 1] &&
-			(newStateOfTiles[rowNum][colNum + 1] = !newStateOfTiles[rowNum][
-				colNum + 1
-			]);
-		newStateOfTiles[colNum - 1] &&
-			(newStateOfTiles[rowNum][colNum - 1] = !newStateOfTiles[rowNum][
-				colNum - 1
-			]);
+    this.props.stopTimer();
+    const score = +this.state.clicks * 10 + this.props.time + 11;
+    this.setState({
+      score: score
+    });
 
-		this.setState((previousState) => {
-			// setClicks(previousState.clicks + 1);
-			this.props.incrementClicks();
-			return {
-				...this.state,
-				litEhStateArr: newStateOfTiles,
-				clicks: previousState.clicks + 1
-			};
-		});
 
-		let countLit = this.state.litEhStateArr.flat().filter((x) => x === true)
-			.length;
+    
+    const gameObject = {
+      clicks: this.state.clicks + 1,
+      time: this.props.time + 1,
+      score: score,
+      level: 1,
+      user_id: this.props.user.id
+    };
+    const fetchOptions = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ game: gameObject })
+    };
 
-		if (countLit === 11) {
-			console.log('you won!');
-			this.props.stopTimer();
-		}
-	};
+    fetch("http://localhost:3000/api/v1/games", fetchOptions)
+  };
 
-	componentDidMount() {
-		const { cols, rows } = this.props;
-		this.buildStateArr(cols, rows);
-	}
+  toggleLit = tileId => {
+    const rowNum = +tileId.split("")[0];
+    const colNum = +tileId.split("")[1];
 
-	render() {
-		const styles = {
-			gridTemplateColumns: `repeat(${this.props.cols}, 3.6rem)`,
-			gridTemplateRows: `repeat(${this.props.rows}, 3.6rem)`
-		};
+    const newStateOfTiles = this.state.litEhStateArr;
+    newStateOfTiles[rowNum][colNum] = !newStateOfTiles[rowNum][colNum];
+    newStateOfTiles[rowNum + 1] &&
+      (newStateOfTiles[rowNum + 1][colNum] = !newStateOfTiles[rowNum + 1][
+        colNum
+      ]);
+    newStateOfTiles[rowNum - 1] &&
+      (newStateOfTiles[rowNum - 1][colNum] = !newStateOfTiles[rowNum - 1][
+        colNum
+      ]);
+    newStateOfTiles[colNum + 1] &&
+      (newStateOfTiles[rowNum][colNum + 1] = !newStateOfTiles[rowNum][
+        colNum + 1
+      ]);
+    newStateOfTiles[colNum - 1] &&
+      (newStateOfTiles[rowNum][colNum - 1] = !newStateOfTiles[rowNum][
+        colNum - 1
+      ]);
 
-		const { litEhStateArr } = this.state;
+    this.setState(previousState => {
+      // setClicks(previousState.clicks + 1);
+      this.props.incrementClicks();
+      return {
+        ...this.state,
+        litEhStateArr: newStateOfTiles,
+        clicks: previousState.clicks + 1
+      };
+    });
 
-		return (
-			<div className={'grid'} id={'tiles__container'} style={styles}>
-				{Array.from(Array(this.props.rows)).map((_tile, rowNum) => {
-					return Array.from(Array(this.props.cols)).map((_tile, colNum) => {
-						return (
-							<Tile
-								key={`${rowNum}${colNum}`}
-								id={`${rowNum}${colNum}`}
-								litEh={litEhStateArr[rowNum] && litEhStateArr[rowNum][colNum]}
-								toggleLit={this.toggleLit}
-							/>
-						);
-					});
-				})}
-			</div>
-		);
-	}
+    let countLit = this.state.litEhStateArr.flat().filter(x => x === true)
+      .length;
+
+    if (countLit === 11) {
+      this.gameComplete();
+    }
+  };
+
+  componentDidMount() {
+    const { cols, rows } = this.props;
+    this.buildStateArr(cols, rows);
+  }
+
+  render() {
+
+
+    const styles = {
+      gridTemplateColumns: `repeat(${this.props.cols}, 3.6rem)`,
+      gridTemplateRows: `repeat(${this.props.rows}, 3.6rem)`
+    };
+
+    const { litEhStateArr } = this.state;
+
+    return (
+      <div className={"grid"} id={"tiles__container"} style={styles}>
+        {Array.from(Array(this.props.rows)).map((_tile, rowNum) => {
+          return Array.from(Array(this.props.cols)).map((_tile, colNum) => {
+            return (
+              <Tile
+                key={`${rowNum}${colNum}`}
+                id={`${rowNum}${colNum}`}
+                litEh={litEhStateArr[rowNum] && litEhStateArr[rowNum][colNum]}
+                toggleLit={this.toggleLit}
+              />
+            );
+          });
+        })}
+      </div>
+    );
+  }
 }
 
 /*
