bc6c2ed694fdcf73be03c34bcd819ad4e5bb82d9
Yining Wang <yining.wang@utexas.edu>
2020-01-23 21:28:57 -0600

    fixed rank in leaderboard

--- src/views/Leaderboard/index.js
+++ src/views/Leaderboard/index.js
@@ -40,7 +40,7 @@
 // export default Leaderboard;
 
 // ++++++++++++++++++++++++
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -52,128 +52,131 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 const columns = [
-  {
-    id: "",
-    label: "Rank",
-    minWidth: 170,
-    align: "center",
-    format: value => value.toLocaleString()
-  },
-  {
-    id: "username",
-    label: "Username",
-    minWidth: 100,
-    align: "center",
-    format: value => value.toLocaleString()
-  },
-  {
-    id: "clicks",
-    label: "Clicks",
-    minWidth: 170,
-    align: "center",
-    format: value => value.toLocaleString()
-  },
-  {
-    id: "time",
-    label: "Time (seconds)",
-    minWidth: 170,
-    align: "center",
-    format: value => value.toLocaleString()
-  },
-  {
-    id: "score",
-    label: "Score",
-    minWidth: 170,
-    align: "center",
-    format: value => value.toFixed(2)
-  }
+	{
+		id: 'rank',
+		label: 'Rank',
+		minWidth: 170,
+		align: 'center',
+		format: (value) => value.toLocaleString()
+	},
+	{
+		id: 'username',
+		label: 'Username',
+		minWidth: 100,
+		align: 'center',
+		format: (value) => value.toLocaleString()
+	},
+	{
+		id: 'clicks',
+		label: 'Clicks',
+		minWidth: 170,
+		align: 'center',
+		format: (value) => value.toLocaleString()
+	},
+	{
+		id: 'time',
+		label: 'Time (seconds)',
+		minWidth: 170,
+		align: 'center',
+		format: (value) => value.toLocaleString()
+	},
+	{
+		id: 'score',
+		label: 'Score',
+		minWidth: 170,
+		align: 'center',
+		format: (value) => value.toFixed(2)
+	}
 ];
 
 const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
+	root: {
+		width: '100%'
+	},
+	container: {
+		maxHeight: 440
+	}
 });
 
 export default function StickyHeadTable() {
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [rows, setRows] = React.useState([])
-  
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-  
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-  
-  const sortGames = (games) => {
-    games.sort((a, b) => {
-      return a.score - b.score;
-    })
-    setRows(games);
-  }
-  
-  useEffect(() => {
-   fetch("http://localhost:3000/api/v1/games")
-    .then(res => res.json())
-    .then(games => sortGames(games));
-}, [])
-    
-  return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map(row => {
-                  return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map(column => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
-}
+	const classes = useStyles();
+	const [page, setPage] = React.useState(0);
+	const [rowsPerPage, setRowsPerPage] = React.useState(10);
+	const [rows, setRows] = React.useState([]);
 
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
+
+	const handleChangeRowsPerPage = (event) => {
+		setRowsPerPage(+event.target.value);
+		setPage(0);
+	};
+
+	const sortGames = (games) => {
+		games.sort((a, b) => {
+			return a.score - b.score;
+		});
+		games.forEach((game, index) => (game.rank = index + 1));
+		console.log(games);
+		setRows(games);
+	};
+
+	useEffect(() => {
+		fetch('http://localhost:3000/api/v1/games')
+			.then((res) => res.json())
+			.then((games) => sortGames(games));
+	}, []);
+
+	return (
+		<Paper className={classes.root}>
+			<TableContainer className={classes.container}>
+				<Table stickyHeader aria-label="sticky table">
+					<TableHead>
+						<TableRow>
+							{columns.map((column) => (
+								<TableCell
+									key={column.id}
+									align={column.align}
+									style={{ minWidth: column.minWidth }}
+								>
+									{column.label}
+								</TableCell>
+							))}
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{rows
+							.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+							.map((row) => {
+								return (
+									<TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+										{/* <TableCell>{index + 1}</TableCell> */}
+										{columns.map((column) => {
+											const value = row[column.id];
+											return (
+												<TableCell key={column.id} align={column.align}>
+													{column.format && typeof value === 'number'
+														? column.format(value)
+														: value}
+												</TableCell>
+											);
+										})}
+									</TableRow>
+								);
+							})}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<TablePagination
+				rowsPerPageOptions={[5, 10, 25, 100]}
+				component="div"
+				count={rows.length}
+				rowsPerPage={rowsPerPage}
+				page={page}
+				onChangePage={handleChangePage}
+				onChangeRowsPerPage={handleChangeRowsPerPage}
+			/>
+		</Paper>
+	);
+}
